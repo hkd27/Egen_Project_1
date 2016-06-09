d7c6ca030b265847fc3bb53f580932273732a6f4
INDIA <hdave@mail.bradley.edu>
2016-06-08 20:59:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/*function init(file, callback) {
+  
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    } 
+    
+   
+    rawFile.send(null);
+}*/
+
+
+window.onload=function(){
+  /*console.log("Hello from insdide");
+  init("movielist.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);*/
+/*});*/
+  
+  
+
+};
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+```CODE_NAME:-Veronica```
+```VERSION:-0.0.1(05/22/2016)```
+
+   EGEN Project 1
